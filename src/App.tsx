b7fb0d98db75ec23a04c302be8aeb25f50bc724d
wesleymarchi <wesleymarchi.x3@gmail.com>
2023-06-30 13:36:56 -0300

    routes

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import LandingPage from "./Pages/LandingPage/LandingPage";
+
+function App(): JSX.Element {
+
+  return (
+    <>
+      <LandingPage />
+    </>
+  )
+}
+
+export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -6,12 +6,6 @@ import reportWebVitals from './reportWebVitals';
 import LandingPage from './Pages/LandingPage/LandingPage';
 import LoginPage from './Pages/LoginPage/LoginPage';
 import RegisterPage from './Pages/RegisterPage/RegisterPage';
-
-import ProtectorModal from "./components/ProtectorList/NewProtectorModal/NewProtectorModal"
-import ProtectorList from './components/ProtectorList/ProtectorList';
-import Register from './components/Login/Register/Register';
-import AssociateList from './components/AssociateList/AssociateList';
-import SupplierList from './components/SupplierList/SupplierList';
 import AssociateListPage from './Pages/AssociateListPage/AssociateListPage';
 import SupplierListPage from './Pages/SupplierListPage/SupplierListPage';
 import ProtectorListPage from './Pages/ProtectorListPage/ProtectorListPage';
